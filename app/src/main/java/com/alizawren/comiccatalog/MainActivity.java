dc407ca492e7442928dd988965c01a92f044dde6
LegendOfPork <skytalker9@gmail.com>
2019-04-17 22:16:46 -0700

    json requests

--- app/src/main/java/com/alizawren/comiccatalog/MainActivity.java
+++ app/src/main/java/com/alizawren/comiccatalog/MainActivity.java
@@ -2,6 +2,7 @@ package com.alizawren.comiccatalog;
 
 import android.Manifest;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -12,6 +13,7 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.hardware.Camera;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.BaseColumns;
 import android.support.annotation.NonNull;
@@ -54,17 +56,27 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     //this will store whether or not we were granted camera permissions
     String[] appPermissions = {
             Manifest.permission.CAMERA,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET
     };
     private int PERMISSIONS_REQUEST_CODE = 1;
 
@@ -170,13 +182,15 @@ public class MainActivity extends AppCompatActivity {
 
                         String text = "Number of barcodes detected: " + barcodes.size() + "\n";
 
+                        String isbn = null;
+
                         for (int index = 0; index < barcodes.size(); index++) {
                             FirebaseVisionBarcode barcode = barcodes.get(index);
                             Rect bounds = barcode.getBoundingBox();
                             Point[] corners = barcode.getCornerPoints();
 
                             String rawValue = barcode.getRawValue();
-
+                            isbn = rawValue;
                             text = text + "Barcode " + (index+1) + ": " + rawValue + "\n";
 
                             int valueType = barcode.getValueType();
@@ -193,6 +207,7 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                         txtView.setText(text);
+                        new GetJSONTask().execute("http://openlibrary.org/api/volumes/brief/isbn/"+isbn+".json");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -346,5 +361,55 @@ public class MainActivity extends AppCompatActivity {
         //if everything is already requested, continue
         Toast.makeText(this,"All Permissions Granted",Toast.LENGTH_SHORT).show();
         return true;
+    }
+
+    private class GetJSONTask extends AsyncTask<String, Void, String> {
+        private ProgressDialog pd;
+        @Override
+        protected void onPreExecute(){
+            pd = ProgressDialog.show(MainActivity.this,"","Loading",true,false);
+        }
+        @Override
+        protected String doInBackground(String... urls){
+//            String url = "http://openlibrary.org/api/volumes/brief/isbn/"+isbnCode+".json";
+            StringBuffer response = null;
+            String url = urls[0];
+            int responseCode = 0;
+            try{
+                URL openlibrary = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) openlibrary.openConnection();
+                connection.setRequestMethod("GET");
+                responseCode = connection.getResponseCode();
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                connection.getInputStream()));
+                String inputLine;
+
+                response = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+            } catch (Exception e) {
+                return("Error when retrieving data, response code:" + responseCode + "\nerror message: " + e);
+            }
+            return response.toString();
+        }
+        @Override
+        protected void onPostExecute(String result){
+            pd.dismiss();
+            //do something with the result...
+            try{
+                JSONObject book = new JSONObject(result);
+                JSONObject bookObject = book.getJSONObject("records").getJSONObject(book.getJSONObject("records").keys().next());
+                String placeHolder = "Record URL: "+bookObject.getString("recordURL")+"\n"+
+                        "ISBN: "+bookObject.getJSONArray("isbns").getString(0)+"\n"+
+                        "Title: "+bookObject.getJSONObject("data").getString("title");
+                txtView.setText(placeHolder);
+            } catch (Exception e){
+                txtView.setText("Can't parse JSON: " + e);
+            }
+
+        }
     }
 }
